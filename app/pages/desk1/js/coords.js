abe511acc931dee7307ab65853db206e89eda3b1
Malekov Azat <Malekov_Azat@outlook.com>
2018-12-19 16:10:11 +0300

    Добавление картинок и создание структуры

---
+++ app/pages/desk1/js/coords.js
@@ -0,0 +1,42 @@
+;(
+    function(){
+        //лампы
+        var lamps = {
+
+        };
+
+        //наковальни
+        var anvils = [
+            '614,314,112,127',
+            '754,314,112,127',
+            '873,314,112,127',
+        ];
+
+        //черные кнопки
+        var black = [
+            '',
+        ];
+
+        //красные кнопки
+        var red = [
+            '',
+        ];
+
+        //тумбелры
+        var tumbs = [
+           '506,576,56,55',
+           '429,576,56,55',
+           '350,577,56,55',
+           '785,104,56,55',
+           '842,107,56,55',
+           '524,316,56,55',
+           '428,398,56,55',
+        ];
+        
+        window.coords = {};
+        window.coords.lamps = lamps;
+        window.coords.anvils = anvils;
+        window.coords.red = red;
+        window.coords.tumbs = tumbs;
+    }
+)();
---
+++ app/pages/desk1/js/main.js
@@ -0,0 +1,28 @@
+;(
+    function(){
+    //     var a = document.getElementById('pointer1');
+    //     var a1 = document.getElementById('pointer2');
+    //     var [angle1,angle2] = [0,0];
+    //     var rotateId1,rotateId2;
+    //     document.getElementById('but').addEventListener('mousedown',function(){
+
+    //         rotateId1 = setInterval(function(){
+    //             angle1 += 1;
+    //             a.style.transform = `rotate(${angle1}deg)`;
+    //             console.log(angle1);
+    //         },10);
+
+    //         rotateId2 = setInterval(function(){
+    //             angle2 += 1;
+    //             a1.style.transform = `rotate(${angle2}deg)`;
+    //             console.log(angle2);
+    //         },20);
+            
+    //     });
+
+    //     document.getElementById('but').addEventListener('mouseup',function(){
+    //         clearInterval(rotateId1);
+    //         clearInterval(rotateId2);
+    //   });
+    }
+)();
