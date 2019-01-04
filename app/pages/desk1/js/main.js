d07bb7a1a00ec25501deac022efa682f38d54d31
Malekov Azat <Malekov_Azat@outlook.com>
2019-01-04 23:59:33 +0300

    Добавление кода для создания кнопок и картинок

--- app/pages/desk1/js/main.js
+++ app/pages/desk1/js/main.js
@@ -1,28 +1,35 @@
 ;(
     function(){
-    //     var a = document.getElementById('pointer1');
-    //     var a1 = document.getElementById('pointer2');
-    //     var [angle1,angle2] = [0,0];
-    //     var rotateId1,rotateId2;
-    //     document.getElementById('but').addEventListener('mousedown',function(){
 
-    //         rotateId1 = setInterval(function(){
-    //             angle1 += 1;
-    //             a.style.transform = `rotate(${angle1}deg)`;
-    //             console.log(angle1);
-    //         },10);
+        //кнопки
+        buttons = сreateAndPlantElem({
+            parentElem:{
+                tagName:'div',
+                idName:'buttons',
+                className:''
+            },
+            childElem:{
+                tagName:'button',
+                className:'cristal-button',
+                constructor:buttonClass,
+            },
+            coords: coords.cristalButtons,
+        });
 
-    //         rotateId2 = setInterval(function(){
-    //             angle2 += 1;
-    //             a1.style.transform = `rotate(${angle2}deg)`;
-    //             console.log(angle2);
-    //         },20);
-            
-    //     });
-
-    //     document.getElementById('but').addEventListener('mouseup',function(){
-    //         clearInterval(rotateId1);
-    //         clearInterval(rotateId2);
-    //   });
+        //лампы
+        lamps = сreateAndPlantElem({
+            parentElem:{
+                tagName:'div',
+                idName:'lamps',
+                className:'',
+            },
+            childElem:{
+                tagName:'img',
+                className:'lamp',
+                mode:'0',
+                constructor:variationPictureClass,
+            },
+            coords:coords.lamps,
+        });
     }
 )();
---
+++ app/pages/items/js/elements.js
@@ -0,0 +1,86 @@
+;(function(){
+
+    
+    /* settings = {
+        parentElem:{ родительский элемент
+            tagName:имя тега ('buttons', 'img')
+            idName: тег id если нужно
+            className:какие классы из CSS добавить
+        },
+        childElem:{
+            tagName:имя тега
+            className:класс для CSS
+            coords:Координаты в порядке top,left,height,width
+            number: номер элемента среди подобных
+            constructor: конструктор для класса
+            ...
+            Другие элементы
+        }
+    } */    
+    //абстрактный класс
+    function myElement(settings){
+        //сам элемент в DOM
+        var elem;
+        function createElement(tagName,className, coord) {
+            this.elem = document.createElement(tagName);
+            this.elem.classList.add(className);
+            var [left, top, width, height] = coord.split(',');
+            var style = this.elem.style;
+            //задание размеров и позиционирования
+            [style.top, style.left, style.height, style.width] = [top + 'px', left + 'px', height + 'px', width + 'px'];
+        };
+        this.number = settings.number;//получаем номер виджета
+        this.elem = elem;
+        this.createElement = createElement;
+    };
+
+    function buttonClass(settings){
+        //функциональное наследование
+        myElement.call(this, settings);//вызов конструктора родителя
+        this.createElement(settings.tagName,settings.className, settings.coord);
+    };
+
+    //общий класс для наковален тумблеров и лампочек
+    function variationPictureClass(settings){
+        myElement.call(this, settings);
+        this.createElement(settings.tagName, settings.className, settings.coord);
+
+        this.mode = settings.mode; //начальное положение
+        // this.deskNumber = settings.deskNumber;//номер пульта
+
+        function setMode(mode){
+            this.elem.setAttribute('src', `images/buttons/Lamp${this.number}_mode_${mode}.png`);
+            // this.elem.setAttribute('src', `/images/buttons/Lamp${0}_mode_${0}.png`);
+        };
+
+        this.setMode = setMode;
+        this.setMode(0);
+    };
+
+
+    function сreateAndPlantElem(settings){
+
+        var parentInfo = settings.parentElem;//родитель
+        var childInfo = settings.childElem;//ребенок
+        var coords = settings.coords;//координаты
+
+        var parent = document.getElementById(parentInfo.idName);
+        var elements = [];
+
+        for(var i = 0 ; i < settings.coords.length; i++){
+            childInfo.number = i;
+            childInfo.coord = coords[i];
+            elements.push(new childInfo.constructor(childInfo) );
+            parent.appendChild(elements[i].elem);
+        }
+        return elements;
+    };
+
+    //конструкторы 
+    window.buttonClass = buttonClass;
+    window.variationPictureClass = variationPictureClass;
+
+    //декоратор для создания элементов
+    window.сreateAndPlantElem = сreateAndPlantElem;
+    
+})();
