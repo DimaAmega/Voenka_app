7daebcff5a53840b7ded8c34451e25144862a03c
DimaAmega <dima.amega1@gmai.com>
2019-03-17 21:25:49 +0300

    добавлен режим обучения

--- app/pages/desk1/js/main.js
+++ app/pages/desk1/js/main.js
@@ -82,230 +82,6 @@
     //Последовательность
     //////////////////////////
 
-var settings = {
-    elements: {
-        speedometrs: [
-            speed1,
-            speed2,
-            speed3,
-        ],
-        buttons:
-            buttons,
-        lamps:
-            lamps,
-    },
-    queue:[
-        {
-            eventObject:'button',
-            number: 5,
-            changeObject:{
-                arrLamps:[
-                    {
-                        num:0,
-                        mode:2
-                    },
-                    {
-                        num:4,
-                    mode:1
-                    },
-                ],
-                //добавить потом тумберы
-            }
-        },
-        {
-            eventObject:'button',
-            number: 18,
-            changeObject:{
-            video:{
-                options:{
-                    src:'videos/videoOpen.mp4',
-                    width:500,
-                    height:500,
-                },
-            }
-            }
-        },
-        {
-            eventObject:'video',
-            changeObject:{},
-            number: 0,
-        },
-        {
-            eventObject:'button',
-            number: 17,
-            changeObject:{
-                arrSpedometrs:[
-                    {
-                        speedNumber: 0,
-                        endAngle: 70,
-                        speed: 20,
-                        OnRight: 0.7,
-                    }
-                ],
-            },
-        },
-        {
-            eventObject:'speedo',
-            number: 0,
-            changeObject: {
-                arrLamps: [
-                    {
-                        num: 7,
-                        mode: 2,
-                    },
-                    {
-                        num: 9,
-                        mode: 2,
-                    },
-                ],
-            }
-        },
-        {
-            eventObject:'button',
-            number:16,
-            changeObject: {
-                arrSpedometrs: [
-                    {
-                        speedNumber: 0,
-                        endAngle: 0,
-                        speed: 20,
-                        OnRight: -0.7,
-                    }
-                ],
-            },
-        },
-        {
-            eventObject: 'speedo',
-            number: 0,
-            changeObject: {
-                arrLamps: [
-                    {
-                        num: 4,
-                        mode: 2,
-                    },
-                    {
-                        num: 13,
-                        mode: 2,
-                    },
-                ],
-            }
-        },
-        {
-        eventObject: 'button',
-        number: 15,
-        changeObject: {
-            arrSpedometrs: [
-                {
-                    speedNumber: 0,
-                    endAngle: 285,
-                    speed: 20,
-                    OnRight: 1,
-                },
-                {
-                    speedNumber: 1,
-                    endAngle: 270,
-                    speed: 20,
-                    OnRight: 0.8,
-                }
-            ],
-        },
-        },
-        {
-            eventObject: 'speedo',
-            number: 1,
-            changeObject: {
-            }
-        },
-        {
-            eventObject: 'speedo',
-            number: 0,
-            changeObject: {
-                arrLamps: [
-                    {
-                        num: 14,
-                        mode: 2,
-                    },
-                    {
-                        num: 15,
-                        mode: 2,
-                    },
-                ],
-            }
-        },
-        {
-            eventObject: 'button',
-            number: 25,
-            changeObject: {
-                arrSpedometrs: [
-                    {
-                        speedNumber: 2,
-                        endAngle: 360,
-                        speed: 120,
-                        OnRight:4,
-                    },
-                ],
-
-            }
-        },
-        {
-            eventObject: 'speedo',
-            number: 2,
-            changeObject: {
-            }
-        },
-        {
-            eventObject: 'button',
-            number: 28,
-            changeObject: {
-                arrSpedometrs: [
-                    {
-                        speedNumber: 2,
-                        endAngle: 320,
-                        speed: 120,
-                        OnRight: -1,
-                    },
-                ],
-            }
-        },
-        {
-            eventObject: 'speedo',
-            number: 2,
-            changeObject: {
-            }
-        },
-        {
-            eventObject: 'button',
-            number: 29,
-            changeObject: {
-                arrSpedometrs: [
-                    {
-                        speedNumber: 2,
-                        endAngle: 300,
-                        speed: 120,
-                        OnRight: -1,
-                    },
-                ],
-            }
-        },
-        {
-            eventObject: 'speedo',
-            number: 2,
-            changeObject: {
-            }
-        },
-        {
-            eventObject: 'button',
-            number: 32,
-            changeObject: {
-            }
-        },
-
-
-        
-    ],
-}
-
-
 /*
 В объекте настроек есть массив queque - это очередь - последоваательность. 
 В этом массиве содержатся объекты, описывающие определённые этапы.
@@ -505,8 +281,7 @@ var settings_true = {
         {
             eventObject: 'speedo',
             number: 2,
-            changeObject: {
-            }
+            changeObject: { }
         },
         {
             eventObject: 'button',
@@ -525,8 +300,7 @@ var settings_true = {
         {
             eventObject: 'speedo',
             number: 2,
-            changeObject: {
-            }
+            changeObject: { }
         },
         {
             eventObject: 'button',
@@ -548,13 +322,23 @@ var settings_true = {
                 ],
              }
         },
-
-    ]
-    
+    ],
+    LearningMessege: ['Кнопка 1 Эта кнопка предназначена для...',
+                    'Кнопка 2 Эта кнопка предназначена для...',
+                    'Кнопка 3 Эта кнопка предназначена для...',
+                    'Кнопка 4 Эта кнопка предназначена для...',
+                    'Кнопка 5 Эта кнопка предназначена для...',
+                    'Кнопка 6 Эта кнопка предназначена для...',
+                    'Кнопка 7 Эта кнопка предназначена для...',
+                    'Кнопка 8 Эта кнопка предназначена для...',
+                    'Кнопка 9 Эта кнопка предназначена для...',
+                    'Кнопка 10 Эта кнопка предназначена для...',
+                    'Кнопка 11 Эта кнопка предназначена для...'],
 }
 
 
-var  seq2 = new Sequence(settings_true);
+
+var  seq2 = new Sequence2(settings_true);
 
 }
 )();
--- app/pages/items/js/sequence.js
+++ app/pages/items/js/sequence.js
@@ -1,7 +1,7 @@
 ;(function (){
 
-
-    function Sequence(settings){
+    function Sequence(settings)
+    {
         var parentElement = document.getElementById('parentElement');
         var currentIteration = 0;
         var allButtons = settings.elements.buttons;
@@ -12,6 +12,8 @@
         var PointsForError = settings.PointsForError;
         var timer = new Timer();
         var check = new Check(ListPointsOfMark);
+
+
         
     ///////////////////////////////////////////////////////////////
     //Служебные функции для обработки очереди в последовательности
@@ -24,7 +26,11 @@
                 allLamps[currentLamps[i].num].setMode(currentLamps[i].mode);
             }
         };
-
+        function getMessage() {
+            return `Молодцы, ваше время -  ${timer.stopTimer()} секунды <br>
+            Ваша оценка - ${check.getMark()} <br>
+            Вы совершили  - ${check.getMistake()} ошибок`;
+        };
         function showVideo(currentIteration) {
             var video;
             if (queue[currentIteration].changeObject.video) video = queue[currentIteration].changeObject.video;
@@ -36,8 +42,21 @@
                 v_obj.play();
             }
             
-        }
-
+        };
+        function showDialog(message) {
+            $('#dialog').html(message).dialog({
+                modal:true,
+                draggable:false,
+                buttons: [{text: "В Меню", click:function() {
+                    $(this).dialog("close");
+                    //редирект на главную
+                }}],
+                width:500,
+                resizable:false,
+                show: { effect: "blind",duration: 1000 },
+                hide: { effect: "explode", duration: 1000 },
+            });
+        };
         function activateSpeedo(currentIteration) {
             var currentSpeedometers = [];
 
@@ -51,7 +70,6 @@
                 }).startRotation();
             }
         };
-
     //////////////////////////////////////////////////////////
     //Установка всех кнопок на клик обращаться к обработчику
     //////////////////////////////////////////////////////////
@@ -62,7 +80,7 @@
             }
         }
     //////////////////////////////////////////////////////////
-    //Сам обработчик - сердце програмым
+    //Сам обработчик - сердце программы
     //////////////////////////////////////////////////////////
         parentElement.addEventListener('myEvent', function(event){
 
@@ -73,36 +91,181 @@
                 activateSpeedo(currentIteration);
                 showVideo(currentIteration);
 
-                
                 if(currentIteration==0) timer.startTimer();
-                if(currentIteration==queue.length-1) {
+                if(currentIteration==queue.length-1) showDialog(getMessage());
 
-                    var messege = `Молодцы, ваше время -  ${timer.stopTimer()} секунды <br>
-                    Ваша оценка - ${check.getMark()} <br>
-                    Вы совершили  - ${check.getMistake()} ошибок`;
-                    
-                    $('#dialog').html(messege).dialog({
-                        modal:true,
-                        draggable:false,
-                        buttons: [{text: "В Меню", click:function() {
-                            $(this).dialog("close");
-                            //редирект на главную
-                        }}],
-                        width:500,
-                        resizable:false,
-                        show: { effect: "fold",duration: 1000 },
-                        hide: { effect: "explode", duration: 1000 }
-                    });
-                };
 
-                ++currentIteration;
+
+                ++currentIteration; 
             }
             else{
                 check.addMistake(PointsForError[currentIteration]);
+                if(check.getMark()==2)  showDialog("Завалился");
+            }  
+        });
+
+    };
+
+
+
+    function Sequence2(settings)
+    {
+        var parentElement = document.getElementById('parentElement');
+        var currentIteration = 0;
+        var allButtons = settings.elements.buttons;
+        var allLamps = settings.elements.lamps;
+        var queue = settings.queue;
+        var allSpedometers = settings.elements.speedometrs;
+        var prevIteration = 0;
+        var LearningMessege = settings.LearningMessege;
+        var elemToolTip = document.getElementById('tooltip');
+        var butCount = 0;
+
+
+        
+    ///////////////////////////////////////////////////////////////
+    //Служебные функции для обработки очереди в последовательности
+    ///////////////////////////////////////////////////////////////
+        
+        function setModeForLamps(currentIteration) {
+            var currentLamps = [];
+            if (queue[currentIteration].changeObject.arrLamps) currentLamps = queue[currentIteration].changeObject.arrLamps;
+
+            for (var i = 0; i < currentLamps.length; i++) {
+                allLamps[currentLamps[i].num].setMode(currentLamps[i].mode);
+            }
+        };
+        function getMessage() {
+            return `Молодцы, вы успешно прошли режим обучения`;
+        };
+        function showVideo(currentIteration) {
+            var video;
+            if (queue[currentIteration].changeObject.video) video = queue[currentIteration].changeObject.video;
+
+            if (video) {
+                var v_obj = new Video(video.options);
+                document.getElementById('videos').appendChild(v_obj.getElem());
+                v_obj.setPosition();
+                v_obj.play();
+            }
+            
+        };
+        function showDialog2(message) {
+            $('#dialog').html(message).dialog({
+                modal:true,
+                draggable:false,
+                buttons: [{text: "В Меню", click:function() {
+                    $(this).dialog("close");
+                    //редирект на главную
+                }}],
+                width:500,
+                resizable:false,
+                show: { effect: "blind",duration: 1000 },
+                hide: { effect: "explode", duration: 1000 },
+            });
+        };
+        function showDialog(message) {
+            $('#dialog').html(message).dialog({
+                modal:true,
+                draggable:false,
+                buttons: [{text: "Хорошо", click:function() {
+                    $(this).dialog("close");
+                    //редирект на главную
+                }}],
+                width:500,
+                resizable:false,
+                show: { effect: "blind",duration: 1000 },
+                hide: { effect: "explode", duration: 1000 },
+            });
+        };
+        function activateSpeedo(currentIteration) {
+            var currentSpeedometers = [];
+
+            if (queue[currentIteration].changeObject.arrSpedometrs) currentSpeedometers = queue[currentIteration].changeObject.arrSpedometrs;
+
+            for (var i = 0; i < currentSpeedometers.length; i++) {
+                allSpedometers[currentSpeedometers[i].speedNumber].endRotation().setParametrsOfMoving({
+                    speed: currentSpeedometers[i].speed,
+                    OnRight: currentSpeedometers[i].OnRight,
+                    endAngle: currentSpeedometers[i].endAngle,
+                }).startRotation();
+            }
+        };
+        function setPositionTooltip(x,y) {
+            elemToolTip.style.left = x;
+            // elemToolTip.style.top = `${Number(y.slice(0,-2))-100}px`;
+            elemToolTip.style.top = y;
+
+        }
+    //////////////////////////////////////////////////////////
+    //Установка всех кнопок на клик обращаться к обработчику
+    //////////////////////////////////////////////////////////
+        for(var i=0;i < allButtons.length;i++){
+            allButtons[i].elem.onclick = function(event){
+                var event = new Event('myEvent', { 'bubbles': true, cancelable:false});
+                this.dispatchEvent(event);
+            }
+        }
+
+
+
+        allButtons[queue[0].number].elem.style.animation = 'clickOnMe 1s infinite ease-in-out';
+        setPositionTooltip(allButtons[queue[0].number].elem.style.left,allButtons[queue[0].number].elem.style.top);
+        elemToolTip.innerHTML = LearningMessege[butCount];
+        elemToolTip.style.transform = 'translate(0,-120px)';
+
+
+
+    //////////////////////////////////////////////////////////
+    //Сам обработчик - сердце программы
+    //////////////////////////////////////////////////////////
+        parentElement.addEventListener('myEvent', function(event){
+
+            if (queue[currentIteration].number == event.target.number &&
+             queue[currentIteration].eventObject == event.target.objectName){
+
+                setModeForLamps(currentIteration);
+                activateSpeedo(currentIteration);
+                showVideo(currentIteration);
+
+                if(currentIteration==queue.length-1){
+                    showDialog2(getMessage());
+                } 
+
+                ++currentIteration; 
+
+                if(currentIteration!=queue.length&&queue[currentIteration].eventObject=='button'){
+                    butCount++;
+
+                    elemToolTip.style.opacity = 1;
+                    elemToolTip.style.transform = 'translate(0,-120px)';
+                    elemToolTip.innerHTML = LearningMessege[butCount];
+
+
+
+                    allButtons[queue[currentIteration].number].elem.style.animation = 'clickOnMe 1s infinite ease-in-out';
+                    allButtons[queue[prevIteration].number].elem.style.animation = '';
+                    prevIteration = currentIteration;
+
+                    setPositionTooltip(allButtons[queue[currentIteration].number].elem.style.left,allButtons[queue[currentIteration].number].elem.style.top);
+
+                }
+                else {
+                    elemToolTip.style.opacity = 0;
+                    elemToolTip.style.transform = 'translate(0,0px)';
+
+                    allButtons[queue[prevIteration].number].elem.style.animation = '';
+                }
+
+            }
+            else{
+                showDialog('Вы совершили ошибку, не нажимайте на неподсвеченные клавиши!')
             }  
         });
 
     };
 
     window.Sequence = Sequence;
+    window.Sequence2 = Sequence2;
+
 })();
